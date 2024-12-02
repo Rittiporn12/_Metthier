1d1577427fc9172b90280e6cdd5bf2affda36ba9
Rittiporn12 <rittiporn.phu@spumail.net>
2024-12-02 18:32:00 +0700

    Delete Sorce Codes

--- src/Data/findzone.jsx
+++
@@ -1,7 +0,0 @@
-const findzone = {
-    A: { name: 'A', parking: '50', limit: '150'},
-    B: { name: 'B', parking: '230', limit: '300'},
-    C: { name: 'C', parking: '450', limit: '500'},
-}
-
-export default findzone;
